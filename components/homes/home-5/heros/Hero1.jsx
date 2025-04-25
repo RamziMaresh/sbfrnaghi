0e14e6d84fb74b2653478f8046697908dda4725b
RamziMaresh <ramzi_maresh@outlook.com>
2025-04-25 18:42:46 +0300

    hggh

--- components/homes/home-5/heros/Hero1.jsx
+++ components/homes/home-5/heros/Hero1.jsx
@@ -3,14 +3,14 @@ import React from "react";
 
 export default function Hero1() {
   return (
-    <div className="container d-flex align-items-center pt-100 pb-100 pt-sm-120 pb-sm-120">
+    <div className="container min-height-70vh d-flex align-items-center pt-100 pb-100 pt-sm-120 pb-sm-120">
       {/* Home Section Content */}
       <div className="home-content text-center">
         <h2 className="section-title-tiny mb-50 mb-sm-30 wow fadeInDownShort">
           Hello &amp; Welcome
         </h2>
         <h1 className="hs-title-3 mb-80 mb-sm-50 mb-xs-30">
-          <span className="wow charsAnimInLong" data-splitting="chars">
+          <span className="wo" data-splitting="chars">
             <AnimatedText text="Sameer  for petrochemical Trading" />
           </span>
         </h1>
